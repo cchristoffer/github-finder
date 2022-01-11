42442d5f4beddae0d5396f9471c92a28804f43fb
Christoffer Helgemo <cchristoffer@live.se>
2022-01-11 22:38:58 +0100

    Clean up

--- src/context/github/GithubContex.js
+++ src/context/github/GithubContex.js
@@ -8,12 +8,13 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   }
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
   const fetchUsers = async () => {
+    setLoading()
     const response = await fetch(`${GITHUB_URL}/users`)
     const data = await response.json()
 
@@ -22,6 +23,8 @@ export const GithubProvider = ({ children }) => {
       payload: data,
     })
   }
+
+  const setLoading = () => dispatch({ type: 'SET_LOADING' })
 
   return (
     <GithubContext.Provider
